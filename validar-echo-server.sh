df1ba65c80d5ceff99ddac48c03a98ba6e7bd0c5
caropico <caropico00@gmail.com>
2025-08-23 15:45:08 -0300

    fixing name on docker compose and network

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mensaje="hola"
-respuesta=$(docker run --rm --network tp0-base_testing_net alpine sh -c "echo '$mensaje' | nc server 12345")
+respuesta=$(docker run --rm --network tp0_testing_net alpine sh -c "echo '$mensaje' | nc server 12345")
 
 if [ "$mensaje" = "$respuesta" ]; then
     echo "action: test_echo_server | result: success"
