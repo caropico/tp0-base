664123fe0aba4e14b8df9c473b036d549ce4e477
caropico <caropico00@gmail.com>
2025-08-23 15:19:21 -0300

    fix on commands

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mensaje="hola"
-respuesta=$(echo "$mensaje" | docker run --rm -i --network tp0_testing_net alpine nc server 12345)
+respuesta=$(docker run --rm --network tp0_testing_net alpine sh -c "echo '$mensaje' | nc server 12345")
 
 if [ "$mensaje" = "$respuesta" ]; then
     echo "action: test_echo_server | result: success"
