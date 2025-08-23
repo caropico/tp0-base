87adbec0e8c0efe95e618de6b5f809cfa13519fd
caropico <caropico00@gmail.com>
2025-08-23 00:01:04 -0300

    fix name of network

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-respuesta=$(echo "hola" | docker run --rm -i --network tp0-base_testing_net alpine nc server 12345)
+mensaje="hola"
+respuesta=$(echo "$mensaje" | docker run --rm -i --network tp0_testing_net alpine nc server 12345)
 
 if [ "$mensaje" = "$respuesta" ]; then
     echo "Echo server funciona correctamente"
