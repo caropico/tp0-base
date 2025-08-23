4ac323db84ad36c0aded2fd3ec62239e7a727724
caropico <caropico00@gmail.com>
2025-08-22 23:25:44 -0300

    deleting logs to fix debug error on tests

--- mi-generador.py
+++ mi-generador.py
@@ -12,7 +12,6 @@ services:
     entrypoint: python3 /main.py
     environment:
       - PYTHONUNBUFFERED=1
-      - LOGGING_LEVEL=DEBUG
     networks:
       - testing_net
 """
@@ -26,7 +25,6 @@ services:
     entrypoint: /client
     environment:
       - CLI_ID={i}
-      - CLI_LOG_LEVEL=DEBUG
     networks:
       - testing_net
     depends_on:
